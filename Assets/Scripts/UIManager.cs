a02efdd3002100136216d014151c3a5bc40fbc36
Saarg <milsonneau.jean@free.fr>
2018-10-28 17:54:03 +0000

    Pause manu + fixed some colliders

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using VehicleBehaviour;
 
+using MOSC;
+
 public class UIManager : MonoBehaviour {
 
 	[SerializeField] WheelVehicle _playerCar;
@@ -18,12 +20,19 @@ public class UIManager : MonoBehaviour {
 
 	[SerializeField] StandaloneInputModule[] _eventSystems;
 
+	[SerializeField] GameObject _pauseMenu;
+
 	void Start()
 	{
 		foreach (StandaloneInputModule es in _eventSystems)
 		{
 			es.enabled = false;
 		}
+
+		_finishText.gameObject.SetActive(false);
+		_recordText.gameObject.SetActive(false);
+
+		_pauseMenu.SetActive(false);
 	}
 	
 	void Update ()
@@ -31,6 +40,35 @@ public class UIManager : MonoBehaviour {
 		_textScore.text = "Score: " + _score;
 
 		_boostBar.fillAmount = _playerCar.Boost / _playerCar.MaxBoost;
+
+		if (MultiOSControls.GetValue("Pause") != 0)
+		{
+			Pause();
+		}
+	}
+
+	void OnApplicationFocus(bool hasFocus)
+    {
+        Pause();
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        Pause();
+    }
+
+	public void Pause()
+	{
+		_pauseMenu.SetActive(true);
+
+		Time.timeScale = 0f;
+	}
+
+	public void Resume()
+	{
+		_pauseMenu.SetActive(false);
+
+		Time.timeScale = 1f;
 	}
 
 	public void AddScore(int score)
